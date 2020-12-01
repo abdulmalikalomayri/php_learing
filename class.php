5b0ebdb3a64071b397b155f9c3d502ba1c0a02b5
Mikami9700 <mlouk9700@gmail.com>
2020-12-01 13:35:24 +0300

    Start

---
+++ class.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class User {
+
+    protected $name;
+
+    function __construct($name) {
+
+        $this->name = $name;
+    }
+}
---
+++ polymorphism.php
@@ -0,0 +1,10 @@
+<?php 
+
+class Rectangle implements shapesInterface {
+
+
+    public function getArea() {
+        
+        return "Rectangle Shape";
+    }
+}
